a6186b6b8344a99d9d55e40ee03b4d836cd833fd
ghostjat <ghost.jat@gmail.com>
2021-01-01 12:42:15 +0530

    Revert "matrix & list example added"
    This reverts commit 026a2de6698c0bf1b814cb03f1a647a4009bb5a0.

--- examples/colorbrowser.php
+++ examples/colorbrowser.php
@@ -11,8 +11,8 @@ $text_green = $iup->text();
 $text_blue = $iup->text();
 
 $clrbrwsr = $iup->colorBrowser();
-#$iup->setCallback($clrbrwsr, "CHANGE_CB", "clrbrwsr_change_cb");
-$iup->setCallback($clrbrwsr, "DRAG_CB", FFI::cast(FFI::typeof($iup->ffi_iup->new("IFnccc")),$fn));
+$iup->setCallback($clrbrwsr, "CHANGE_CB", "clrbrwsr_change_cb");
+$iup->setCallback($clrbrwsr, "DRAG_CB","clrbrwsr_drag_cb");
 $vbox = $iup->ffi_iup->IupVbox($iup->fill(), $text_red, $iup->fill(), $text_green, $iup->fill(), $text_blue, $iup->fill(), null);
 $hbox_final = $iup->ffi_iup->IupHbox($clrbrwsr, $iup->fill(), $vbox, null);
 $dlg = $iup->dialog($hbox_final);
@@ -22,25 +22,19 @@ $iup->showXY($dlg, $iup::CENTER, $iup::CENTER);
 $iup->mainLoop();
 $iup->close();
 
-function clrbrwsr_change_cb($ih,$r,$g,$b) {
+function clrbrwsr_change_cb(...$opts) {
+    global $iup, $clrbrwsr;
+    $iup->getAttribute($clrbrwsr, "VALUE");
+    return $iup::DEFAULT;
+}
+
+function clrbrwsr_drag_cb(...$opts) {
     global $iup, $text_red, $text_green, $text_blue;
     
-    $iup->setAttribute($text_red, "VALUE", (string) $r);
-    $iup->setAttribute($text_green, "VALUE", (string) $g);
-    $iup->setAttribute($text_blue, "VALUE", (string)$b);
+    $iup->setAttribute($text_red, "VALUE", $r);
+    $iup->setAttribute($text_green, "VALUE", $g);
+    $iup->setAttribute($text_blue, "VALUE", $b);
     
     $iup->loopStep();
     $iup::DEFAULT;
 }
-$fn = FFI::new('int');
-
-$fn->cdata = function ($ih,$r,$g,$b) {
-    global $iup, $text_red, $text_green, $text_blue;
-    
-    $iup->setAttribute($text_red, "VALUE", (string) $r);
-    $iup->setAttribute($text_green, "VALUE", (string) $g);
-    $iup->setAttribute($text_blue, "VALUE", (string)$b);
-    
-    $iup->loopStep();
-    $iup::DEFAULT;
-};
--- src/iup.php
+++ src/iup.php
@@ -248,7 +248,7 @@ class core {
     public function string(FFI\CData $data){
         return \FFI::string($data);
     }
-    
+
 
     protected function ImageLibOpen() {
         return $this->ffi_imgLib->IupImageLibOpen();
@@ -535,9 +535,6 @@ class core {
         return $this->ffi_iup->IupFlatSeparator();
     }
     
-    /**
-     * Fill the object
-     */
     public function fill() {
         return $this->ffi_iup->IupFill();
     }
@@ -650,17 +647,6 @@ class core {
     
     public function getClassName($ih){
         return $this->ffi_iup->IupGetClassName($ih);
-    }
-    
-    /**
-     * 
-     * @param string $classname
-     * @param string $names
-     * @param int $max_n
-     * @return type
-     */
-    public function getClassCallbacks($classname, $names, $max_n) {
-        return $this->ffi_iup->IupGetClassCallbacks($classname,$names,$max_n);
     }
     
     public function getClassType($ih){
